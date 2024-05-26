08b5443a124188a4e0479dae8892857c9dbb55d7
Arfan Abid <iffionex111@gmail.com>
2024-05-26 23:57:33 +0500

    DataBase : Connected MYSQL DB

--- Backend/core/core/settings.py
+++ Backend/core/core/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Blog_db',  # Name database according to your choice
+        'USER': 'root',
+        'PASSWORD': '*******', #you set while Settingup MySQL
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
